399c138a68196b83d4bf916c9f29d9b6851b107e
root <esiebomagoodness@gmail.com>
2022-03-11 00:46:11 -0800

    yt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
- gcc -o cisun $CFILE
+#include <stdio.h>
+/**
+ *  * main - main block
+ *   * Return: 0
+ *    */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
