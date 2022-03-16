bb21d688119d0ae156b571cf8ab61b1f4a0f58a0
root <esiebomagoodness@gmail.com>
2022-03-15 20:57:56 -0700

    lk

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
  #include "main.h"
+
  /**
-  * print_sign - prints the sign of a number
-  * @n: interger to be tested
-  * Return: 1, 0, -1
-  */
+   * print_sign - prints the sign of a number
+   * @n: interger to be tested
+   * Return: 1, 0, -1
+   */
 int print_sign(int n)
 
 {
