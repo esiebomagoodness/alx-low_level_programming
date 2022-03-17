544181c3f2c5f5b2d81b2131273651c00f39354a
root <esiebomagoodness@gmail.com>
2022-03-16 21:11:40 -0700

    df

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,27 @@
-#include "main.h"                                    
-/**                                                    
- * print_sign - prints the sign of a number          
- * @n: integer to be checked   
- * Return: 1,0 or -1                            
- */                                                  
-int print_sign(int n)                                
-{                                                      
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: integer to be checked
+ * Return: 1,0 or -1
+ */
+int print_sign(int n)
+{
 	int value;
 
-	if (n > 0)      
-	{          		
-		value = 1;        	
-		_putchar('+');                      
-	}                          
-	else if (n == 0)                      
-	{                                           
-		value = 0;                 
-		_putchar('0');              
-	}                                          
-	else                          
-	{              
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
 		value = -1;
-		_putchar('-');              
+		_putchar('-');
 	}
 	return (value);
 }
