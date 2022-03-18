cf87c5170d56bb69f3c97a9ee89be6f86e2303c4
root <esiebomagoodness@gmail.com>
2022-03-17 18:11:28 -0700

    th

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,6 @@ void print_triangle(int size)
 		_putchar('\n');
 	for (row = 0; row < size; row++)
 	{
-
 		for (column = size - row; column > 1; column--)
 			_putchar(' ');
 		for (k = row + column; k >= 1; k--)
