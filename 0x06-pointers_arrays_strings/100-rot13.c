608f325f6c10bd51b3ee6a1114329d82c18e4bca
root <esiebomagoodness@gmail.com>
2022-03-24 13:04:27 -0700

    grt

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,7 +12,7 @@ char *rot13(char *s)
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *ptr = s;
-	
+
 	while (*s)
 	{
 		for (i = 0; i <= 52; i++)
